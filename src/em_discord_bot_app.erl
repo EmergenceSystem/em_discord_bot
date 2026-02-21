1eefb2c0a8cc3442aaf9505057ff97106569d38b
Steve Roques <steve.roques@gmail.com>
2026-02-21 20:50:47 +0100

    remove jsx

--- src/em_discord_bot_app.erl
+++ src/em_discord_bot_app.erl
@@ -1,12 +1,17 @@
+%%%-------------------------------------------------------------------
+%%% @doc Emergence Discord bot application.
+%%% @end
+%%%-------------------------------------------------------------------
 -module(em_discord_bot_app).
 -behaviour(application).
+
 -export([start/2, stop/1]).
 
 start(_Type, _Args) ->
-    ok = application:set_env(discord_bot_light, command_handler, em_discord_bot_commands),
+    ok = application:set_env(discord_bot_light, command_handler,
+                             em_discord_bot_commands),
     {ok, _} = application:ensure_all_started(discord_bot_light),
     {ok, self()}.
 
 stop(_State) ->
     ok.
-
--- src/em_discord_bot_commands.erl
+++ src/em_discord_bot_commands.erl
@@ -1,116 +1,62 @@
+%%%-------------------------------------------------------------------
+%%% @doc Discord bot command handler for Emergence queries.
+%%% @end
+%%%-------------------------------------------------------------------
 -module(em_discord_bot_commands).
+
 -export([handle_message/4]).
 
--include_lib("embryo/include/embryo.hrl").
+%%====================================================================
+%% Message handler
+%%====================================================================
 
-%%--------------------------------------------------------------------
-%% Main message handler
-%%--------------------------------------------------------------------
 handle_message(Content, ChannelId, _Author, Token) ->
-    SearchQuery = Content,
-    io:format("Search Query: ~p~n", [SearchQuery]),
-
-    Results = query(SearchQuery),
-    case Results of
+    Body = iolist_to_binary(
+        json:encode(#{<<"value">> => unicode:characters_to_binary(Content)})),
+    case query(Body) of
+        {ok, Embryos} ->
+            Msg = format_message(Embryos),
+            discord_bot_light_client:send_message(ChannelId, iolist_to_binary(Msg), Token);
         {error, Reason} ->
-            ErrorMsg = io_lib:format("Search error: ~p", [Reason]),
-            io:format("Sending Error Message: ~p~n", [ErrorMsg]),
-            discord_bot_light_client:send_message(ChannelId, iolist_to_binary(ErrorMsg), Token);
-        {ok, MessageList} ->
-            FormattedMessage = format_message(MessageList),
-            discord_bot_light_client:send_message(ChannelId, iolist_to_binary(FormattedMessage), Token)
+            Err = io_lib:format("Search error: ~p", [Reason]),
+            discord_bot_light_client:send_message(ChannelId, iolist_to_binary(Err), Token)
     end.
 
-%% Function to format the message list into a single string
-format_message(MessageList) ->
-    TruncatedList = lists:sublist(MessageList, 5),
-    lists:map(fun(Message) ->
-        Url = get_url(Message),
-        Description = get_description(Message),
-        io_lib:format("~s~n~s~n~n", [Url, Description])
-    end, TruncatedList).
+%%====================================================================
+%% Formatting
+%%====================================================================
 
-%% Function to get the URL and description from the map
-get_embryo_info(EmbryoMap) ->
-    Props = maps:get(<<"properties">>, EmbryoMap, #{}),
-    Url = maps:get(<<"url">>, Props, ""),
-    Description = maps:get(<<"description">>, Props, ""),
-    #{url => Url, description => Description}.
+format_message(Embryos) ->
+    lists:map(fun(E) ->
+        Props = maps:get(<<"properties">>, E, #{}),
+        Url   = maps:get(<<"url">>,    Props, <<>>),
+        Desc  = maps:get(<<"resume">>, Props, <<>>),
+        io_lib:format("~ts~n~ts~n~n", [Url, Desc])
+    end, lists:sublist(Embryos, 5)).
 
-%%%-------------------------------------------------------------------
-%%% Query Logic
-%%%-------------------------------------------------------------------
-query(Search) ->
+%%====================================================================
+%% HTTP query
+%%====================================================================
+
+query(Body) ->
     ServerUrl = embryo:get_em_disco_url(),
-    io:format("Server URL: ~s~n", [ServerUrl]),
-    handle_query(ServerUrl, Search).
-
-%%%-------------------------------------------------------------------
-%%% Send query and handle response
-%%%-------------------------------------------------------------------
-handle_query(ServerUrl, Query) ->
-    case post_query(ServerUrl, Query) of
-        {ok, Body} ->
-            handle_response(Body);
-        {error, Reason} ->
-            io:format("Failed to send HTTP request to ~s : ~p~n", [ServerUrl, Reason]),
-            {error, Reason}
-    end.
-
-%% Function to perform a POST query
-post_query(ServerUrl, Query) ->
-    URL = ServerUrl ++ "/query",
-    Headers = [{"content-type", "application/json"}],
-    Body = Query,
-    case httpc:request(post, {URL, Headers, "application/json", Body}, [], [{body_format, binary}]) of
-        {ok, {{_, 200, _}, _RespHeaders, RespBody}} ->
-            {ok, binary_to_list(RespBody)};
+    Url       = ServerUrl ++ "/query",
+    Headers   = [{"content-type", "application/json"}],
+    case httpc:request(post, {Url, Headers, "application/json", Body},
+                       [], [{body_format, binary}]) of
+        {ok, {{_, 200, _}, _, RespBody}} ->
+            handle_response(RespBody);
         {ok, {{_, Status, _}, _, RespBody}} ->
-            io:format("Failed to get successful response. Status code: ~p~n", [Status]),
-            {error, binary_to_list(RespBody)};
+            io:format("HTTP ~p from ~s~n", [Status, Url]),
+            {error, RespBody};
         {error, Reason} ->
             {error, Reason}
     end.
 
-%%%-------------------------------------------------------------------
-%%% Response handling
-%%%-------------------------------------------------------------------
 handle_response(Body) ->
-    case jsx:is_json(list_to_binary(Body)) of
-        true ->
-            try jsx:decode(list_to_binary(Body), [return_maps]) of
-                #{<<"embryo_list">> := EmbryoList} ->
-                    Messages = lists:map(fun get_embryo_info/1, EmbryoList),
-                    {ok, Messages};
-                _ ->
-                    {ok, [Body]}
-            catch
-                _:_ ->
-                    {ok, [Body]}
-            end;
-        false ->
-            {ok, [Body]}
+    try json:decode(Body) of
+        #{<<"embryo_list">> := EmbryoList} -> {ok, EmbryoList};
+        _                                  -> {ok, []}
+    catch
+        _:_ -> {error, invalid_json}
     end.
-
-%% Function to get the description
-get_description(Message) ->
-    case Message of
-        #{description := Description} when is_binary(Description) ->
-            binary_to_list(Description);
-        #{description := Description} ->
-            Description;
-        _ ->
-            "No Description"
-    end.
-
-%% Function to get the URL
-get_url(Message) ->
-    case Message of
-        #{url := Url} when is_binary(Url) ->
-            binary_to_list(Url);
-        #{url := Url} ->
-            Url;
-        _ ->
-            "No URL"
-    end.
-
